65cfa113376e9dbbddd052f71190c5799d39e400
evomassiny <hanzohatoryv@gmail.com>
2021-12-04 11:37:21 +0100

    Added clox

---
+++ clox/chunk.h
@@ -0,0 +1,9 @@
+#ifndef clox_chunk.h 
+#define clox_chunk.h 
+#include "common.h"
+
+typedef enum {
+    OP_RETURN,
+} OpCode;
+
+#endif
---
+++ clox/main.c
@@ -0,0 +1,6 @@
+#include "common.h"
+
+int main(int argc, char ** argv) {
+    printf("It's been a while...");
+    return 0;
+}
